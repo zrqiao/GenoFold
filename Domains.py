af76733d028c6d835259a47e5f98893c72fc7f69
Zhuoran Qiao <utenaq@pku.edu.cn>
2018-08-03 16:08:38 -0400

    Interface finished

--- Domains.py
+++ Domains.py
@@ -1,9 +1,9 @@
-import numpy as np
-import scipy as sp
 import nupack_functions
 from difflib import SequenceMatcher
 import numpy as np
 from collections import defaultdict
+import operator
+from multiprocessing import Pool
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import matplotlib as mpl
@@ -29,58 +29,83 @@ def similar(a, b):
 def rate(dG):
     return k0*np.exp(-(k*dG))
 
-class domains_collection():
 
+class DomainsCollection(object):
     def __init__(self):
-        self.collection = defaultdict(dict)
+        self.collection = dict()
 
-    def get_domain(self, sequence, structure, lbound, rbound):#Get another domain from collection or create a new one
-        key = (sequence, structure, lbound, rbound)
+    def __contains__(self, adomain):
+        if (adomain.sequence, adomain.structure, adomain.l_bound, adomain.r_bound) in self.collection.keys():
+            return True
+        else:
+            return False
+
+    def get_domain(self, sequence, structure, l_bound, r_bound):#Get another domain from collection or create a new one
+        key = (sequence, structure, l_bound, r_bound)
         if key in self.collection.keys():
             return self.collection[key]
         else:
-            return domain(sequence, structure, lbound, rbound, self.collection)
+            new_domain = Domain(sequence, structure, l_bound, r_bound, self)
+            self.add_domain(new_domain)  # Add to repository when created
+            return new_domain
 
     def add_domain(self, adomain):
-        self.collection[(adomain.sequence, adomain.structure, adomain.lbound, adomain.rbound)] = adomain  # Add to repository when created
+        self.collection[(adomain.sequence, adomain.structure, adomain.l_bound, adomain.r_bound)] = adomain  # Add to repository when created
 
 
-def get_foldon(sequence, lbound, rbound, domain_collection, foldon_collection):#foldon_collection[lbound][rbound]=object(foldon)
-    mfe = nupack_functions.nupack_mfe(sequence, Temperature)
-    newfoldon = domain_collection.get_domain(sequence, mfe, lbound, rbound)
-    newfoldon.foldonize()
-    if newfoldon not in foldon_collection[lbound].values():
-        foldon_collection[lbound][rbound] = newfoldon
-    return newfoldon
+class FoldonCollection(object):
+    def __init__(self):
+        self.collection = defaultdict(defaultdict(set))
 
-class domain(object):
-    def __init__(self, sequence, structure, lbound, rbound, collection, population=0):
+    def add_foldon(self, adomain):
+        if adomain.is_foldon:
+            self.collection[adomain.l_bound][adomain.r_bound].add(adomain)  # Add to repository when created
+            # TODO: degenerate case
+            return self
+        else:
+            return False
+
+    def get_foldons(self, l_bound, r_bound):  # Get another domain from collection, cannot create new instances
+        try:
+            return self.collection[l_bound][r_bound]
+        except IndexError:
+            print("Error: no such foldon")
+            return False
+
+    def new_foldon(self, sequence, l_bound, r_bound, domain_collection):#foldon_collection is a DomainCollection
+        mfe = nupack_functions.nupack_mfe(sequence, Temperature)
+        new_foldon = domain_collection.get_domain(sequence, mfe, l_bound, r_bound)
+        new_foldon.foldonize()
+        if new_foldon not in self.collection[l_bound][r_bound]:
+            self.add_foldon(new_foldon)
+        return new_foldon
+
+class Domain(object):
+    def __init__(self, sequence, structure, l_bound, r_bound, collection):
         # [ , ) rbound[i]==lbound[i+1](stopping base +1)
         self.sequence = sequence
         self.structure = structure
-        self.lbound = lbound
-        self.rbound = rbound
-        self.length = rbound-lbound
+        self.l_bound = l_bound
+        self.r_bound = r_bound
+        self.length = r_bound - l_bound
         self.reducible = True
         self.G = None
         self.elements = set()
-        self.isfoldon = False
-        self.population = population
+        self.is_foldon = False
         self.IFR = None #Irreducible foldons representation NOTE: IFR is automatically generated when initiate foldons
         self.collection = collection #NOTE: collection is a domains_collection
-        self.collection.add_domain(self) #Add to repository when created
 
     def __eq__(self, other):
-        return self.structure == other.structure and self.lbound == other.lbound and self.rbound == other.rbound
+        return self.structure == other.structure and self.l_bound == other.lbound and self.r_bound == other.rbound
 
     def __hash__(self):
-        return hash((self.sequence, self.structure, self.lbound, self.rbound))
+        return hash((self.sequence, self.structure, self.l_bound, self.r_bound))
 
     def __str__(self):
         return self.structure
 
     def __repr__(self):
-        return self.lbound, self.rbound, self.structure
+        return self.l_bound, self.r_bound, self.structure
 
     def get_domain(self, sequence, structure, lbound, rbound):#Get another domain from collection or create a new one
         #key = (sequence, structure, lbound, rbound)
@@ -93,40 +118,37 @@ class domain(object):
         return self.structure
 
     def foldonize(self):#If self is a foldon
-        self.isfoldon = True
-        self.IFR = [self.lbound, self.rbound]
+        self.is_foldon = True
+        self.IFR = [self.l_bound, self.r_bound]
         return True
-
-    def change_population(self,population):
-        self.population = population
 
     def get_elements(self): #find closed domains. NOTE: No peudo knots
         if not self.elements:
             unpaired = []
             elements = []
-            sub_lbound = self.lbound
+            sub_l_bound = self.l_bound
             if self.reducible:
                 for index in range(self.length):
                     symb = self.structure[index]
                     base = self.sequence[index]
                     if symb == '.':
                         if not unpaired:
-                            elements.append(self.get_domain(base, symb, self.lbound+index, self.rbound+index+1))
+                            elements.append(self.get_domain(base, symb, self.l_bound + index, self.r_bound + index + 1))
                         else:
                             pass
                     elif symb == '(':
-                        sub_lbound=self.lbound+index
+                        sub_l_bound= self.l_bound + index
                         unpaired.append(symb)
                     elif symb == ')':
                         try:
                             unpaired.pop()
                             if not unpaired:
-                                sub_rbound = self.lbound+index+1
-                                newelement = self.get_domain(self.sequence[sub_lbound:sub_rbound],
-                                                       self.structure[sub_lbound:sub_rbound],
-                                                       sub_lbound, sub_rbound)
-                                newelement.reducible = False
-                                elements.append(newelement)
+                                sub_r_bound = self.l_bound + index + 1
+                                new_element = self.get_domain(self.sequence[sub_l_bound:sub_r_bound],
+                                                       self.structure[sub_l_bound:sub_r_bound],
+                                                       sub_l_bound, sub_r_bound)
+                                new_element.reducible = False
+                                elements.append(new_element)
                         except IndexError: #If ')' appears alone
                             print('Error: Invalid secondary structure')
                     else:
@@ -138,7 +160,7 @@ class domain(object):
             self.elements = set(elements)
         return self.elements
 
-    def calc_G(self):#NOTE: G is not initialized, has to be called explicitly before k calculation
+    def get_G(self):#NOTE: G is not initialized, has to be called explicitly before k calculation
         if not self.G:
             self.G = nupack_functions.nupack_ss_free_energy(sequence=self.sequence, ss=self.structure, T=Temperature)
         return self.G
@@ -153,11 +175,11 @@ class domain(object):
         else:
             if self.structure != '.':#Is a helix?
                 #helix_core = sub_ss.structure[1:-1] #Check here!
-                loopss = '('+'.'*(self.length-2)+')'
+                loop_ss = '('+'.'*(self.length-2)+')'
             else: #Is a '.'?
-                loopss = self.structure
-        loopstate = self.get_domain(self.sequence, ''.join(loopss), self.lbound, self.rbound)
-        return loopstate
+                loop_ss = self.structure
+        loop_state = self.get_domain(self.sequence, ''.join(loop_ss), self.l_bound, self.r_bound)
+        return loop_state
 
     def dissociate_energy(self):
         #NOTE: This version is a very primary estimation for unfolding-folding free energy that did not discriminate
@@ -169,7 +191,7 @@ class domain(object):
         #G_empty=.0
         #dG_forward= G_loop1 - self.calc_G() + G_loop2 - G_empty
         #dG_backward = G_loop2 - other.calc_G() + G_loop1 - G_empty
-        return G_loop-self.calc_G()
+        return G_loop-self.get_G()
 
     def loop_formation_energy(self):
         #NOTE: This version is a very primary estimation for unfolding-folding free energy that did not discriminate
@@ -181,17 +203,20 @@ class domain(object):
         G_loop = self.dissociate_to_loop().calc_G()
         return G_loop-G_empty
 
-    def elongate(self, d_seq, d_ss, dl):#Primary structure have a IFR; elongation structure is a Irreducible Foldon
-        longerdomain= self.get_domain(self.sequence+d_seq, self.structure+d_ss, self.lbound, self.rbound+dl)
-        if not longerdomain.IFR: #update IFR
-            longerdomain.IFR = self.IFR
-            longerdomain.IFR.append(longerdomain.rbound)
-        return longerdomain
+    def elongate(self, d_seq, d_ss, dl):#Primary structure have a IFR; elongation structure is a Irreducible foldon;
+        # generate another valid domain from one
+        longer_domain= self.get_domain(self.sequence + d_seq, self.structure + d_ss, self.l_bound, self.r_bound + dl)
+        if not longer_domain.IFR: #update IFR
+            longer_domain.IFR = self.IFR
+            longer_domain.IFR.append(longer_domain.rbound)
+        return longer_domain
 
     def check_availability(self, other): #Forward rearrangement
         IFRa = set(self.IFR)
         IFRb = set(other.IFR)
-        if IFRa & IFRb == IFRb:
+        if IFRa == IFRb:
+            return False
+        elif IFRa & IFRb == IFRb:
             diff = IFRa ^ IFRb
             for i in range(len(other.IFR)-1):
                 if max(diff) < other.IFR[i+1] and min(diff) > other.IFR[i]:
@@ -199,40 +224,6 @@ class domain(object):
             return False
         else:
             return False
-
-    def pathway_link(self, other, pathways):
-        trial = self.check_availability(other)
-        if trial:
-            sub_lbound, sub_rbound = trial
-            mysite = self.get_domain(self.sequence[sub_lbound:sub_rbound],
-                                                       self.structure[sub_lbound:sub_rbound],
-                                                       sub_lbound, sub_rbound)
-            othersite = other.get_domain(other.sequence[sub_lbound:sub_rbound],
-                                     other.structure[sub_lbound:sub_rbound],
-                                     sub_lbound, sub_rbound)
-            forwardrate = 0; backwardrate = 0
-            #NOTE: rate calculation interface
-            #if already exists
-            forwardrate = pathways.get_rate(mysite, othersite)  # TODO: implementation
-            backwardrate = pathways.get_rate(othersite, mysite)
-            if not forwardrate and backwardrate:
-                nocontributionelements = mysite.get_elements() ^ othersite.get_elements()
-                mycontributions = mysite.get_elements() - nocontributionelements
-                theircontributions = othersite.get_elements() - nocontributionelements
-                forwardenergy = sum(map(lambda x:x.dissociate_energy(), mycontributions)) +\
-                                sum(map(lambda x:x.loop_formation_energy(), theircontributions))
-                backwardenergy = sum(map(lambda x: x.dissociate_energy(), theircontributions)) + \
-                                sum(map(lambda x: x.loop_formation_energy(), mycontributions))
-                forwardrate = rate(forwardenergy)
-                backwardrate = rate(backwardenergy)
-                #newforwardpathway = pathway(mysite,othersite, forwardrate)
-                #newbackwardpathway = pathway(othersite,mysite, backwardrate)
-                pathways.add(mysite, othersite, forwardrate)
-                pathways.add(othersite, mysite, backwardrate)
-            pathways.add(self, other, forwardrate)
-            pathways.add(other, self, backwardrate)
-
-            return forwardrate, backwardrate
         #Add to collection
 
     ###def pathway_rate(self, other, pathway_collection):#Input
@@ -242,19 +233,125 @@ class domain(object):
 #        domain.__init__(self, adomain.sequence, adomain.structure, adomain.lbound, adomain.rbound, foldon_collection)
 #        self.isfoldon = True
 
-class pathways(object):#Indices of a pathway should be two domain(for robustness, can be improved by using IFR for indices)
+class Pathways(object):#Indices of a pathway should be two domain(for robustness, can be improved by using IFR for indices)
     def __init__(self):
-        self.collection = defaultdict(defaultdict(np.float))
+        self.collection = defaultdict(dict)
+
+    def has_path(self, source, sink):
+        if sink in self.collection[source].keys():
+            return True
+        else:
+            return False
 
     def get_rate(self, source, sink):
+        if not self.has_path(source, sink):
+            self.pathway_link(source, sink)
         return self.collection[source][sink]
 
     def add(self, source, sink, rate):
         self.collection[source][sink] = rate
 
+    def pathway_link(self, domain1, domain2):    #If no path exists, call this
+        # NOTE: finding the minimum rearrangement site
+        trial_forward = domain1.check_availability(domain1)
+        if trial_forward:
+            trial = trial_forward
+            source, sink = domain1, domain2
+        else:
+            trial_backward = domain2.check_availability(domain2)
+            if trial_backward:
+                trial = trial_backward
+                source, sink = domain2, domain1
+            else:
+                forward_rate = backward_rate = 0
+                self.add(domain1, domain2, forward_rate)
+                self.add(domain2, domain1, backward_rate)
+                return forward_rate, backward_rate
+        sub_l_bound, sub_r_bound = trial
+        if sub_l_bound==domain1.lbound and sub_r_bound==domain2.rbound:#This is the minimum rearrangement site
+            my_site, other_site= source, sink
+        else:
+            my_site = source.get_domain(source.sequence[sub_l_bound:sub_r_bound],
+                                                       source.structure[sub_l_bound:sub_r_bound],
+                                                       sub_l_bound, sub_r_bound)
+            other_site = sink.get_domain(sink.sequence[sub_l_bound:sub_r_bound],
+                                     sink.structure[sub_l_bound:sub_r_bound],
+                                     sub_l_bound, sub_r_bound)
+        # NOTE: rate calculation interface
+        # if already exists
+        if self.has_path(my_site, other_site):# If calculated
+            forward_rate = self.get_rate(my_site, other_site)
+            backward_rate = self.get_rate(other_site, my_site)
+        else:
+            #Rate calculation
+            no_contribution_elements = my_site.get_elements() ^ other_site.get_elements()
+            my_contributions = my_site.get_elements() - no_contribution_elements
+            their_contributions = other_site.get_elements() - no_contribution_elements
+            forward_energy = sum(map(lambda x: x.dissociate_energy(), my_contributions)) + \
+                            sum(map(lambda x: x.loop_formation_energy(), their_contributions))
+            backward_energy = sum(map(lambda x: x.dissociate_energy(), their_contributions)) + \
+                             sum(map(lambda x: x.loop_formation_energy(), my_contributions))
+            forward_rate = rate(forward_energy)
+            backward_rate = rate(backward_energy)
+            # newforwardpathway = pathway(my_site,other_site, forward_rate)
+            # newbackwardpathway = pathway(other_site,my_site, backward_rate)
+            self.add(my_site, other_site, forward_rate)
+            self.add(other_site, my_site, backward_rate)
+        self.add(source, sink, forward_rate)
+        self.add(sink, source, backward_rate)
+        return forward_rate, backward_rate
+
 pathway_collection = defaultdict(dict)
 
-class species(object):
+class SpeciesPool(object):
     def __init__(self, pathways):
-        self.domains = set()
+        self.species = defaultdict(np.float)
         self.pathways = pathways
+        self.size = 0
+
+    def add_species(self, domain, population=0):
+        if domain.IFR:
+            self.species[domain] = population
+            self.size += 1
+
+    def clear(self):
+        self.species.clear()
+        self.size = 0
+        return self
+
+    def get_population(self, domain):
+        return self.species[domain]
+
+    def update_population(self, domain, population):
+        self.species[domain] = population
+        return self
+
+    def evolution(self, pathways, time):
+        rate_matrix = np.zeros((self.size, self.size))
+        species_list = list(self.species.items())
+        population_array = np.zeros(self.size)
+        # TODO: parallel optimization
+        for i in range(self.size):
+            population_array[i] = species_list[i][1]
+            for j in range(self.size):
+                rate_matrix[i][j] = pathways.get_rate(species_list[i][0], species_list[j][0])
+            rate_matrix[i][i] = -np.sum(rate_matrix[i])
+
+        # Master Equation
+        population_array = population_array.dot(expm(time*rate_matrix))
+
+        # Remapping
+        for i in range(self.size):
+            self.update_population(species_list[i][0], population_array[i])
+
+        return self
+
+
+    def selection(self, size_limit):
+        if self.size > size_limit:
+            ordered_species = list(sorted(self.species.items(), key=operator.itemgetter(1), reverse=True))[:size_limit]
+            remaining_population = sum(list(zip(ordered_species))[1])
+            self.clear()
+            for species in ordered_species:
+                self.add_species(species[0], population=species[1]/remaining_population)
+        return self
--- Folding_Dynamics.py
+++ Folding_Dynamics.py
@@ -5,22 +5,55 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import matplotlib as mpl
 import Domains
+from collections import defaultdict
 import nupack_functions
+import argparse, math, random, gzip, pickle, types
 
-#Change following routines for other environments:
-L_init = 20 #Initiation unit
-dL = 5 #elongation unit (also CG unit)
-dt = 1 #Folding time for each elongation step
+# Change following routines for other environments:
+L_init = 20  # Initiation unit
+dL = 5  # elongation unit (also means CG unit)
+dt = 1  # Folding time for each elongation step
+population_size_limit = 25  # maximum type of strands in the pool
 
-#TODO: Initiation [create active population]
+if __name__ == '__main__':
 
-#TODO: structure_generation(single strain, elgation segment) [to be called in pool.map()]
-#Compute all IFR segments; link sequences; update IFRs
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sequence', type=str, help="RNA sequence (one line)")
+    clargs = parser.parse_args()
+    with open(clargs.sequence, 'r') as sequence_file:
+        full_sequence = sequence_file.readline()
 
-#TODO: population_selection (need a active population, fittness function)
 
-#TODO: compute_foldon(i,j)
+    #NOTE: Initiation [create active population]
+    all_domains = Domains.DomainsCollection()
+    all_foldons = Domains.FoldonCollection()
+    all_pathways = Domains.Pathways()
+    active_species_pool = Domains.SpeciesPool(all_pathways)
 
-#TODO: need a collection for all foldons
+    # NOTE: Initiate transcription
+    init_segment = full_sequence[:L_init]
+    init_foldon = all_foldons.new_foldon(init_segment, 0, L_init, all_domains)
+    active_species_pool.add_species(init_foldon, population=1.0)
+    sequence_length = len(full_sequence)
+    current_length = L_init
 
-#TODO: A active map for all pathways
+    while sequence_length < current_length:
+        if current_length+dL > sequence_length:
+            L_step = sequence_length-current_length
+        else:
+            L_step = dL
+        current_length += L_step
+        # TODO: structure_generation(single strain, elongation segment) [to be called in pool.map()]
+        #Compute all IFR segments; link sequences; update IFRs
+
+        # TODO: active pool update
+
+        # TODO: population_selection (need a active population, fitness function)
+
+        active_species_pool.evolution(all_pathways, dt)
+        active_species_pool.selection(population_size_limit)
+        # NOTE: compute_foldon(i,j)
+
+        # TODO: pickle & outputs
+
+exit()

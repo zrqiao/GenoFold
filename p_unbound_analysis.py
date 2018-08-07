b6cd2f86ceeb26c4f0d3f2923c9500d85cd8ad6c
Zhuoran Qiao <utenaq@pku.edu.cn>
2018-08-07 13:34:03 -0400

    Minor typo fixes

--- p_unbound_analysis.py
+++ p_unbound_analysis.py
@@ -3,61 +3,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import matplotlib as mpl
+import argparse, math, random, gzip, pickle, types
+from collections import defaultdict
+
+
+# Change following routines for other environments:
+L_init = 10  # Initiation unit
+dL = 10  # elongation unit (also means CG unit)
+transcription_time = 0.1
+dt = transcription_time * dL  # Folding time for each elongation step (0.1 s/nt)
+population_size_limit = 100  # maximum type of strands in the pool
+MULTI_PROCESS = 32
+SD_start, SD_end = 22, 29
 
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-plt.style.use('ggplot')
-fig = plt.figure(figsize=(10, 10))
 
-colors = [plt.cm.jet(lt) for lt in range(0,8)]
-fig.add_axes()
+if __name__ == '__main__':
 
-#mpl.rcParams['axes.color_cycle'] = colors
-mpl.rcParams['axes.titlesize']=20
-mpl.rcParams['axes.titleweight'] = 15
-TPTDistribution=np.array([])
+    plt.style.use('ggplot')
+    fig = plt.figure(figsize=(10, 10))
 
+    # colors = [plt.cm.jet(lt) for lt in range(0, 8)]
+    fig.add_axes()
 
-fig = plt.figure(figsize=(10, 7.5))
-fig.add_axes()
-gs = gridspec.GridSpec(2, 1, height_ratios=[1.5, 2.5])
+    # mpl.rcParams['axes.color_cycle'] = colors
+    mpl.rcParams['axes.titlesize'] = 20
+    mpl.rcParams['axes.titleweight'] = 15
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sequence', type=str, help="RNA sequence (one line)")
+    # parser.add_argument('--k', type=np.float, default=1., \
+    #                     help="pre exponential factor")
+    clargs = parser.parse_args()
+    with open(clargs.sequence + '.in', 'r') as sequence_file:
+        full_sequence = sequence_file.readline().rstrip('\n')
 
-ax_energy = fig.add_subplot(gs[0,0])
-ax_energy.set_title('Free Energy')
-#ax_energy.set_xlabel('Subsequence Length', fontsize=12.5)
-ax_energy.set_ylabel('Free Energy', fontsize=12.5)
+    #   NOTE: Initiation [create active population]
 
-ax_structure = fig.add_subplot(gs[1,0])
-ax_structure.set_title('First N nt local structure predominance among subsequences')
-ax_structure.set_xlabel('Subsequence Length', fontsize=12.5)
-ax_structure.set_ylabel('Similarity', fontsize=12.5)
+    # NOTE: Initiate transcription
 
-with open('summary_energy.dat','r') as energy_dat:
-    energy=np.loadtxt(energy_dat)
-    seqcount=np.arange(1,len(energy)+1)
-    ax_energy.plot(seqcount,energy,ls='solid')
+    # print('Population size: '+str(active_species_pool.size))
 
-with open('summary_structure.dat','r') as structure_dat:
-    f=open('Similarity.dat','w')
-    structure=np.array(list(map(lambda x: x.rstrip('\n'),structure_dat.readlines())))
-    seqcount=np.arange(1,len(energy)+1)
-    for N in (np.arange(5,50,5)):
-        print('N=' + str(N) + ' nt')
-        f.write('#N=' + str(N) + ' nt')
-        sim=np.zeros(len(structure))
-        first_sequences=np.array(list(map(lambda x:x[:N],structure[N:])))
-        for i in range(len(first_sequences)):
-            temp_cmp=[]
-            for cmpseq in first_sequences:
-                temp_cmp.append(similar(cmpseq,first_sequences[i]))
-            sim[i+N]=np.average(np.array(temp_cmp))
-            #print(sim)
-        ax_structure.plot(seqcount[N:],sim[N:],ls='solid',label='N='+str(N)+' nt')
-        np.savetxt(f,sim)
-    ax_structure.legend(loc='best')
-fig.tight_layout()
-plt.show()
+    # Start IO
+    fig = plt.figure(figsize=(10, 7.5))
+    fig.add_axes()
+    gs = gridspec.GridSpec(1, 1, height_ratios=[1])
 
-fig.savefig('G&Similarity.eps')
+    #ax_energy = fig.add_subplot(gs[0, 0])
+    #ax_energy.set_title('Free Energy')
+    # ax_energy.set_xlabel('Subsequence Length', fontsize=12.5)
+    #ax_energy.set_ylabel('Free Energy', fontsize=12.5)
+
+    ax_pbound = fig.add_subplot(gs[0, 0])
+    ax_pbound.set_title(f'Average p_unbound for SD sequence {clargs.sequence}')
+    ax_pbound.set_xlabel('Transcription time', fontsize=12.5)
+    ax_pbound.set_ylabel('p_unbound', fontsize=12.5)
+
+    for e_k in range(5, 19):
+        k = 1*10**e_k
+        print(f'k= {k}')
+        data = defaultdict(np.float)
+        with open(clargs.sequence + '_k' + '%e' % k + '.dat', 'r') as folding_input:
+            f = open(clargs.sequence + '_p_unbound_%e' % k + '.dat', 'w')
+            sss = [(x.split()[0].rstrip('\n'), np.float(x.split()[1]))
+                   for x in folding_input.readlines() if not x.startswith('#')]
+
+            for ss in sss:
+                time = len(ss) * transcription_time
+                # print('N=' + str(N) + ' nt')
+                # f.write('#N=' + str(N) + ' nt')
+                # sim = np.zeros(len(structure))
+                SD_ss = ss[SD_start:SD_end]
+                data[time] += similar(SD_ss, '.......')
+                # print(data[time])
+                # for i in range(len(first_sequences)):
+                #     temp_cmp = []
+                #     for cmpseq in first_sequences:
+                #         temp_cmp.append(similar(cmpseq, first_sequences[i]))
+                #     sim[i + N] = np.average(np.array(temp_cmp))
+                # print(sim)
+            data_p = np.array([list(data.keys()), list(data.values())])
+            data_p.transpose()
+
+            ax_pbound.plot(data_p, ls='solid', label='k=' + '%.2g' % k + '$s-1')
+            for d in data.items():
+                f.write(f'{d[0]}  {d[1]}')
+            f.close()
+    ax_pbound.legend(loc='best')
+    fig.tight_layout()
+    plt.show()
+
+    fig.savefig('p_unbound_test_k_tuning.eps')

c1e0e58781eff6693aa2570f027e6ec5712ab072
Zhuoran Qiao <utenaq@pku.edu.cn>
2018-08-20 23:14:25 -0400

    Generated mutants processing files

--- format_mutants.py
+++ format_mutants.py
@@ -1,23 +1,23 @@
-import sys
+import os, sys
 
 
 def rna_seq(sequence):
     # Convert a DNA sequence to a RNA sequence
     return ''.join(c if c != 'T' else 'U' for c in sequence)
 
-UTR = ATACCCGTTTTTTGGGCTAACAGGAGGAATTACAT
+UTR = 'ATACCCGTTTTTTGGGCTAACAGGAGGAATTACAT'
 input = sys.argv[1]
 
-with open(input, r+) as f:
-    for line in f.readlines().strip('\n'):
-        if line.startswith('>')
-            name=sys.argv[2]+'_'+line[1:]
+with open(input, 'r+') as f:
+    for line in f.readlines():
+        if line.startswith('>'):
+            name=sys.argv[2]+'_'+line[1:].strip('\n')
             if not os.path.exists(name):
                 os.makedirs(name)
             with open('mutants.in', 'a') as f1:
                 f1.write(name+'\n')
         else:
-            dnaseq=UTR+line
+            dnaseq=UTR+line.strip('\n')
             rnaseq=rna_seq(dnaseq)
             with open('sequences/'+name+'.in', 'w+') as rnaf:
                 rnaf.write(rnaseq)

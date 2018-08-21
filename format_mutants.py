cae07e9fe27187a8cb218abb0930e30d497cca73
Zhuoran Qiao <utenaq@pku.edu.cn>
2018-08-20 22:58:41 -0400

    Added format_mutants.py

---
+++ format_mutants.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def rna_seq(sequence):
+    # Convert a DNA sequence to a RNA sequence
+    return ''.join(c if c != 'T' else 'U' for c in sequence)
+
+UTR = ATACCCGTTTTTTGGGCTAACAGGAGGAATTACAT
+input = sys.argv[1]
+
+with open(input, r+) as f:
+    for line in f.readlines().strip('\n'):
+        if line.startswith('>')
+            name=sys.argv[2]+'_'+line[1:]
+            if not os.path.exists(name):
+                os.makedirs(name)
+            with open('mutants.in', 'a') as f1:
+                f1.write(name+'\n')
+        else:
+            dnaseq=UTR+line
+            rnaseq=rna_seq(dnaseq)
+            with open('sequences/'+name+'.in', 'w+') as rnaf:
+                rnaf.write(rnaseq)
+    
+        
+
+       

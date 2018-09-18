Plotting adjustment Minor fix: normalization in propagation routines

Analysis/analyze_pbad_correlations.py
@@ -4,13 +4,130 @@ import scipy.stats
 start_base = 21
 end_base = 28
 km_start = 4
-km_end = 19
+km_end = 17
 k_pre = 1e11
 km_interval = 1
 
 import matplotlib
 import matplotlib.pyplot as plt
 # sphinx_gallery_thumbnail_number = 2
+
+
+def func(x, pos):
+    return "{:.2f}".format(x).replace("0.", ".").replace("1.00", "")
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Arguments:
+        data       : A 2D numpy array of shape (N,M)
+        row_labels : A list or array of length N with the labels
+                     for the rows
+        col_labels : A list or array of length M with the labels
+                     for the columns
+    Optional arguments:
+        ax         : A matplotlib.axes.Axes instance to which the heatmap
+                     is plotted. If not provided, use current axes or
+                     create a new one.
+        cbar_kw    : A dictionary with arguments to
+                     :meth:`matplotlib.Figure.colorbar`.
+        cbarlabel  : The label for the colorbar
+    All other arguments are directly passed on to the imshow call.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+    ax.set_yticklabels(row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    #ax.tick_params(top=False, bottom=True, labeltop=False, labelbottom=True)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    for edge, spine in ax.spines.items():
+        spine.set_visible(False)
+
+    # ax.set_xticks(np.arange(data.shape[1]), minor=True)
+    # ax.set_yticks(np.arange(data.shape[0]), minor=True)
+    # ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    # ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["white", "white"],
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Arguments:
+        im         : The AxesImage to be labeled.
+    Optional arguments:
+        data       : Data used to annotate. If None, the image's data is used.
+        valfmt     : The format of the annotations inside the heatmap.
+                     This should either use the string format method, e.g.
+                     "$ {x:.2f}", or be a :class:`matplotlib.ticker.Formatter`.
+        textcolors : A list or array of two color specifications. The first is
+                     used for values below a threshold, the second for those
+                     above.
+        threshold  : Value in data units according to which the colors from
+                     textcolors are applied. If None (the default) uses the
+                     middle of the colormap as separation.
+
+    Further arguments are passed on to the created text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    print(data)
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if data[i, j] < 0 or data[i,j] > 0:
+                kw.update(color=textcolors[im.norm(data[i, j]) > threshold])
+                text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+                texts.append(text)
+
+    return texts
+
 
 def get_pearsonr(x, y, pval=False):
     R, p = scipy.stats.pearsonr(x, y)
@@ -30,8 +147,8 @@ def bootstrap_correlation(x, y, fcn, n=1000):
     z = list(zip(x, y))
     p = 0
     R = np.zeros(n)
-    if not x:
-        return 0, 0, 1
+    if len(x) <= 5:
+        return float('NaN'), float('NaN'), 1
     for i in range(n):
         s = [z[np.random.randint(len(z))] for j in range(len(z))]
         # s = z
@@ -50,15 +167,15 @@ if __name__ == '__main__':
     Ncterm = 3 * 20
     sequences = {}
     trans_rate_es = 11 - (np.arange(km_start, km_end, km_interval))
-    rbs_bind_time_es = list(np.arange(-7, 8, 0.25))
+    rbs_bind_time_es = list(np.arange(-7, 9, 1))
     Nterm_data = np.zeros((len(trans_rate_es), len(rbs_bind_time_es)))
 
     # Calculate ddG
     for e_k in range(km_start, km_end, km_interval):
         k1 = 1 * 10 ** e_k
         k_T = (k_pre/k1)
-        for e_time in np.arange(-7, 8, 0.25):
-            rbs_time = 1 * 10 ** (e_time)
+        for e_time in np.arange(-7, 9, 1):
+            rbs_time = 1 * 10 ** float(e_time)
             print(k_T, rbs_time)
             sequences = {}
             for gene in genes:
@@ -89,7 +206,7 @@ if __name__ == '__main__':
             for k in sorted(data):
                 if k[1] != 'WT':
                     if sequences[k][:Ncterm] != sequences[(k[0], 'WT')][
-                                                :Ncterm]:  # and sequences[k][Ncterm:] == sequences[(k[0], 'WT')][Ncterm:]:
+                                                :Ncterm] and sequences[k][Ncterm:] == sequences[(k[0], 'WT')][Ncterm:]:
                         groups['mut_nterm'].append(k)
                     elif sequences[k][:Ncterm] == sequences[(k[0], 'WT')][:Ncterm] and \
                             sequences[k][Ncterm:] != sequences[(k[0], 'WT')][Ncterm:]:
@@ -100,7 +217,7 @@ if __name__ == '__main__':
             for d in data:
                 # print(d[0] + '_' + d[1] + '/CG5' + '/k' + '%.2g' % k1 + f'/p_unbound/base{start_base}_{end_base}.dat')
                 try:
-                    with open(d[0] + '_' + d[1] + '/CG5_pool50' + '/k' + '%.2g' % k1 + f'/p_unbound/base{start_base}_{end_base}.dat', 'r+') as p_unbound_in:
+                    with open(d[0] + '_' + d[1] + '/CG5_pool50_backup' + '/k' + '%.2g' % k1 + f'/p_unbound/base{start_base}_{end_base}.dat', 'r+') as p_unbound_in:
                         mutant_pub_data = {}
                         for line in p_unbound_in:
                             length, pub = map(float, line.split())
@@ -129,42 +246,53 @@ if __name__ == '__main__':
             print("all %d %6.3f %6.3f %g" % (len(y), R_mean, R_std, p))
             '''
             x, y = [], []
+
             for d in sorted(groups['mut_nterm']):
                 wt_d = (d[0], 'WT')
                 if 'punbound' in data[d] and 'punbound' in data[wt_d] and data[d]['rel_mrna_abundance'] > 0:
-                    x.append(math.log(data[d]['punbound']) - math.log(data[wt_d]['punbound']))
-                    y.append(math.log(data[d]['rel_mrna_abundance']))
+                    if data[d]['punbound'] == 0 or data[wt_d]['punbound'] == 0:
+                        x.append(data[d]['punbound'] - data[wt_d]['punbound'])
+                        y.append(math.log(data[d]['rel_mrna_abundance']))
+                    else:
+                        x.append(data[d]['punbound'] - data[wt_d]['punbound'])
+                        y.append(math.log(data[d]['rel_mrna_abundance']))
+
+            for d in sorted(groups['mut_ncterm']):
+                wt_d = (d[0], 'WT')
+                if 'punbound' in data[d] and 'punbound' in data[wt_d] and data[d]['rel_mrna_abundance'] > 0:
+                    if data[d]['punbound'] == 0 or data[wt_d]['punbound'] == 0:
+                        x.append(data[d]['punbound'] - data[wt_d]['punbound'])
+                        y.append(math.log(data[d]['rel_mrna_abundance']))
+                    else:
+                        x.append(data[d]['punbound'] - data[wt_d]['punbound'])
+                        y.append(math.log(data[d]['rel_mrna_abundance']))
+
+            for d in sorted(groups['mut_cterm']):
+                wt_d = (d[0], 'WT')
+                if 'punbound' in data[d] and 'punbound' in data[wt_d] and data[d]['rel_mrna_abundance'] > 0:
+                    if data[d]['punbound'] == 0 or data[wt_d]['punbound'] == 0:
+                        x.append(data[d]['punbound'] - data[wt_d]['punbound'])
+                        y.append(math.log(data[d]['rel_mrna_abundance']))
+                    else:
+                        x.append(data[d]['punbound'] - data[wt_d]['punbound'])
+                        y.append(math.log(data[d]['rel_mrna_abundance']))
             x_all += x
             y_all += y
             R_mean, R_std, p = bootstrap_correlation(x, y, get_pearsonr)
             print("%s %d %6.3f %6.3f %g" % (d, len(y), R_mean, R_std, p))
-            Nterm_data[int((e_k-km_start)/km_interval)][int(e_time*4)+28] = R_mean
+            Nterm_data[int((e_k-km_start)/km_interval)][int(e_time)+7] = R_mean
 
-
-
+    plt.style.use('seaborn')
+    plt.rcParams["axes.grid"] = False
     fig = plt.figure(figsize=(20, 9))
     # colors = [plt.cm.jet(lt) for lt in range(0, 8)]
     fig.add_axes()
     ax = fig.add_subplot(111)
-    im = ax.imshow(Nterm_data)
-
-    # We want to show all ticks...
-    ax.set_yticks(np.arange(len(trans_rate_es)))
-    ax.set_xticks(np.arange(len(rbs_bind_time_es)))
-    # ... and label them with the respective list entries
-    ax.set_yticklabels(trans_rate_es)
-    ax.set_xticklabels(rbs_bind_time_es)
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
-
-    # Loop over data dimensions and create text annotations.
-    for i in range(len(trans_rate_es)):
-        for j in range(len(rbs_bind_time_es)):
-            text = ax.text(j, i, '%.2f' % Nterm_data[i, j],
-                           ha="center", va="center", color="w")
+    # im = ax.imshow(Nterm_data)
+    im, _ = heatmap(Nterm_data, trans_rate_es, rbs_bind_time_es, ax=ax, vmin=-1, vmax=1,
+                    cmap="RdYlBu_r", cbarlabel="correlation coeff.")
+    texts = annotate_heatmap(im, valfmt=matplotlib.ticker.FuncFormatter(func))
 
     ax.set_title("mRNA - p_unbound(effective RBS binding time) correlation")
     fig.tight_layout()
-    fig.savefig('Analysis/mRNA-p_unbound(effective RBS binding time)correlation_SD-refined_090718.png')
+    fig.savefig('Analysis/mRNA-p_unbound(effective RBS binding time)correlation_n+nc+cterm_-10-_091118.png')

bin/Domains.py
@@ -153,7 +153,7 @@ def Propagate_sp(M, p, dt, ddt=1):
     # E = np.real(np.dot(np.dot(U, np.diag(R)), Uinv))
     for i in range(len(times)):
         p_temp = np.dot(expm(ddt*M), p)
-        p_norm = np.linalg.norm(p_temp)
+        p_norm = np.linalg.norm(p_temp, 1)
         p = p_temp/p_norm
         # A = mp.expm(M.T*times[i], method='pade')*p
         intermediate_populations.append(p)